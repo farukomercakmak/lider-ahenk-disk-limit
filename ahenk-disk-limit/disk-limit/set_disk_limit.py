148452e5cb47951b4ead00ddf97ca82c142e9c3b
Edip YILDIZ <edip.yildiz@hotmail.com>
2017-07-29 11:25:52 +0300

    ahenk disk kullanım oranı zamanlanmış gorevlerde tekrar mail gonderimi önlendi

--- ahenk-disk-limit/disk-limit/set_disk_limit.py
+++ ahenk-disk-limit/disk-limit/set_disk_limit.py
@@ -19,23 +19,34 @@ class DiskLimit(AbstractPlugin):
         values = ['disk_limit', status, self.timestamp()]
         self.db_service.update('mail', cols, values)
 
+    def update_mail(self, status,id):
+        cols = ['mailstatus', 'timestamp'];
+        values = [status,self.timestamp()]
+        self.db_service.update('mail', cols, values,'id='+str(id))
+
+
     def set_mail_content_withusage(self,mail_content,disk_limit,usage):
         if mail_content.__contains__('{limit}'):
-            mail_content = str(mail_content).replace('{limit}', str(disk_limit));
+            mail_content = str(mail_content).replace('{limit}', str(disk_limit['limitation']));
         if mail_content.__contains__('{usage}'):
             mail_content = str(mail_content).replace('{usage}', str(usage));
         if mail_content.__contains__('{ahenk}'):
             mail_content = str(mail_content).replace('{ahenk}', str(self.Ahenk.dn()));
 
+        self.context.set_mail_content(mail_content)
+
     def handle_task(self):
-        disk_limit = self.task['DiskLimitPercentage'];
+        disk_limit = self.task['diskUsageEntity'];
         usage= (self.Hardware.Disk.used()/self.Hardware.Disk.total())*100;
 
-        self.logger.debug('[DiskLimit] usage:  ' + str(usage));
-# usage limit control
-        if self.context.is_mail_send():
+        disk_limit['usage']=usage;
+
+        mail = self.db_service.select('mail', '*', 'command = \'disk_limit\' ')
+        # usage limit control
+
+        if float(disk_limit['limitation']) <= float(disk_limit['usage']) and self.context.is_mail_send():
             self.set_mail_content_withusage(self.context.get_mail_content(), disk_limit, usage)
-            mail = self.db_service.select('mail', '*', 'command = \'disk_limit\' ')
+
             if len(mail) < 1:
                 self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
                                              message='Disk Kullanım Limiti başarıyla oluşturuldu.',
@@ -43,12 +54,14 @@ class DiskLimit(AbstractPlugin):
                                                  'Result': 'İşlem Başarı ile gercekleştirildi',
                                                  'mail_content': str(self.context.get_mail_content()),
                                                  'mail_subject': str(self.context.get_mail_subject()),
-                                                 'mail_send': self.context.is_mail_send()
+                                                 'mail_send': self.context.is_mail_send(),
+                                                 'disk_limit' :disk_limit
                                              }),
                                              content_type=ContentType.APPLICATION_JSON.value)
                 self.save_mail(1)
             else:
                 status = mail[0][2];
+                id= mail[0][0];
                 if status == 0:
                     self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
                                                  message='Disk Kullanım Limiti başarıyla oluşturuldu.',
@@ -56,19 +69,42 @@ class DiskLimit(AbstractPlugin):
                                                      'Result': 'İşlem Başarı ile gercekleştirildi',
                                                      'mail_content': str(self.context.get_mail_content()),
                                                      'mail_subject': str(self.context.get_mail_subject()),
-                                                     'mail_send': self.context.is_mail_send()
+                                                     'mail_send': self.context.is_mail_send(),
+                                                     'disk_limit': disk_limit
                                                  }),
                                                  content_type=ContentType.APPLICATION_JSON.value)
-                    self.save_mail(1)
+                    self.update_mail(1,id)
 
                 else:
                     self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
                                                  message='Disk Kullanım Limiti başarıyla oluşturuldu.',
                                                  data=json.dumps({
                                                      'Result': '',
-                                                     'mail_send': False
+                                                     'mail_send': False,
+                                                     'disk_limit': disk_limit
                                                  }),
                                                  content_type=ContentType.APPLICATION_JSON.value)
+        else :
+            if len(mail) > 0:
+                id = mail[0][0];
+                self.update_mail(0,id)
+                self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
+                                            message='Disk Kullanım Limiti başarıyla oluşturuldu.',
+                                            data=json.dumps({
+                                                'Result': '',
+                                                'mail_send': False,
+                                                'disk_limit': disk_limit
+                                             }),
+                                            content_type=ContentType.APPLICATION_JSON.value)
+            else :
+                self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
+                                             message='Disk Kullanım Limiti başarıyla oluşturuldu.',
+                                             data=json.dumps({
+                                                 'Result': '',
+                                                 'mail_send': False,
+                                                 'disk_limit': disk_limit
+                                             }),
+                                             content_type=ContentType.APPLICATION_JSON.value)
 
 
 
